c395187603d7242ef98fc390b35c361d110fa4fb
Jose Canto <47193615+Josecp77@users.noreply.github.com>
2022-11-08 12:31:12 +0100

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -2,6 +2,7 @@
 import random
 
 guessesTaken = 0
+finded = False
 
 print('Hello! What is your name?')
 myName = input();
@@ -9,19 +10,22 @@ myName = input();
 number = random.randint(1, 20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
-for guessesTaken in range(6):
-    print('Take a guess.')
+print('Take a guess.')
+
+while  finded == False:
+
     guess = input()
     guess = int(guess)
 
     if guess < number:
         print('Your guess is too low')
-
-    if guess > number:
+        guessesTaken = guessesTaken + 1
+    elif guess > number:
         print('Your guess is too high.')
-
-    if guess == number:
-        break
+        guessesTaken = guessesTaken + 1
+    else:
+        finded = True
+    
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
